cd0ae0aae80d40dbe832474893bf3c3e9072bd7d
gilson <gilson@wstudent-79-160-13.solent.ac.uk>
2020-09-28 16:15:21 +0100

    Defined routes for end-points

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Games;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// all of the routes are in the /games end-point
+Route::group(["prefix" => "games"], function () {
+
+    // GET /games: show all games
+    Route::get("", [Games::class, "index"]);
+
+    // POST /games: create a new game
+    Route::post("", [Games::class, "store"]);
+
+    // all these routes also have an game ID in the
+    // end-point, e.g. /games/8
+    Route::group(["prefix" => "{id}"], function () {
+    // GET /games/8: show the game
+        Route::get("", [Games::class, "show"]);
+        // UPDATE /games/8: update the game
+        Route::patch("", [Games::class, "update"]);
+        // DELETE /games/8: delete the game
+        Route::delete("", [Games::class, "destroy"]);
+    });
 });
