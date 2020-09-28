089adf781e74d74bd6ececd0c5490116d422a1a8
gilson <gilson@wstudent-79-160-13.solent.ac.uk>
2020-09-28 16:10:09 +0100

    Defined columns

--- database/migrations/2020_09_28_150459_create_games_table.php
+++ database/migrations/2020_09_28_150459_create_games_table.php
@@ -15,10 +15,19 @@ class CreateGamesTable extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->tinyinteger("complete");
+            $table->integer("winning_score")->default(21);
+            $table->integer("change_server")->default(5);
+            $table->string("player_1");
+            $table->string("player_2");
+            $table->integer('player_1_score')->default(0);
+            $table->integer('player_2_score')->default(0);
             $table->timestamps();
+
+            $table->foreignId("players_id")->unsigned();
+            $table->foreign("players_id")->references("id")->on("players");
         });
     }
-
     /**
      * Reverse the migrations.
      *
